38615072fef06e3975a72a19621480c69a5474e4
_kbluue_ <kbluue@yahoo.com>
2019-09-05 19:02:52 +0100

    Notes: +1

--- app/src/main/java/com/example/kbluue_/unnamedtaskapp/Models/Timestamp.java
+++ app/src/main/java/com/example/kbluue_/unnamedtaskapp/Models/Timestamp.java
@@ -8,7 +8,15 @@ import java.util.TimeZone;
 
 public class Timestamp {
 
-    protected static final String REGEX = "-";
+    static final String REGEX = "-";
+    static final String CONTROL_FORMAT = "d-MM-YY";
+    static final String TODAY_FORMAT = "hh:mm a";
+    static final String YESTERDAY = "Yesterday";
+    static final String DAY_OF_WEEK_FORMAT = "EEEE";
+    static final String DAYS_AGO = " days ago";
+    static final String SAME_YEAR_FORMAT = "MMM d";
+    static final String DIFFERENT_DAY_FORMAT = "d-MMM-YY";
+
     public final Date date;
 
     public Timestamp(){
@@ -31,25 +39,25 @@ public class Timestamp {
     }
 
     private String getFormat(){
-        SimpleDateFormat format = new SimpleDateFormat("d-MM-YY", Locale.ENGLISH);
+        SimpleDateFormat format = new SimpleDateFormat(CONTROL_FORMAT, Locale.ENGLISH);
         format.setTimeZone(TimeZone.getDefault());
         String[] d1 = format.format(new Date()).split(REGEX),
                 d2 = format.format(getDate()).split(REGEX);
         if (Arrays.equals(d1, d2)){
-            return "hh:mm a";
+            return TODAY_FORMAT;
         } else if (d1[2].equals(d2[2]) && d1[1].equals(d2[1])){
             int daysApart = Integer.parseInt(d1[0]) - Integer.parseInt(d2[0]);
             if (daysApart == 1){
-                return "Yesterday";
+                return YESTERDAY;
             } else if (daysApart < 7){
-                return "EEEE";
+                return DAY_OF_WEEK_FORMAT;
             } else {
-                return daysApart + " days ago";
+                return daysApart + DAYS_AGO;
             }
         } else if (d1[2].equals(d2[2])) {
-            return "MMM d";
+            return SAME_YEAR_FORMAT;
         } else {
-            return "d-MMM-YY";
+            return DIFFERENT_DAY_FORMAT;
         }
     }
 }
